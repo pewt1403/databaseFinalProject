c1e3bd99f8cac37b068977544479b26a3c8eb2c9
pewt1403 <p_ngamboontham@hotmail.com>
2019-11-02 18:02:53 +0700

    clone

--- parcelManagement/views.py
+++ parcelManagement/views.py
@@ -4,8 +4,10 @@ from django.template import loader
 # Create your views here.
 def index(request):
 	header_str = 'Use template file'
+	tableContent = 
 	template = loader.get_template('parcelmanage.html')
 	context = {
 		'var1' : header_str
+		'tc' : 
 	}
 	return HttpResponse(template.render(context,request))
